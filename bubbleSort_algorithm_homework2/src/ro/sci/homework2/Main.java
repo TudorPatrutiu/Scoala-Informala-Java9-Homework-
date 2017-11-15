73c592a54b1703bac72413d7741b732010ef6bd1
TUDOR\Tudor Patrutiu <patrutiu.tudor@gmail.com>
2017-11-15 22:53:59 +0200

    Added the .class files to .gitignore and the algorithm is written. Still need to figure a way to call the method in Main.

---
+++ bubbleSort_algorithm_homework2/src/ro/sci/homework2/Main.java
@@ -0,0 +1,12 @@
+package ro.sci.homework2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BubbleSortReps sortedReps = new BubbleSortReps();
+        int[] reps = sortedReps.reps;
+        System.out.println(sortedReps);
+    }
+}
+
